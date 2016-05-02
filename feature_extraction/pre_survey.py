8a0ef680d7db77b0e69c1e4be2b465882ab6abcc
Ruomiao Guo <guorm@DN51p025.SUNet>
2016-05-01 23:43:43 -0700

    add hours planned to spend

--- feature_extraction/pre_survey.py
+++ feature_extraction/pre_survey.py
@@ -3,10 +3,13 @@ from collections import defaultdict
 
 survey_id_courses_started = {}
 survey_id_courses_finished = {}
+survey_id_hours_spent = {}
 courses_started_dict = {0:0}
 courses_finished_dict = {0:0}
-screen_name_courses_started = defaultdict(int)
-screen_name_courses_finished = defaultdict(int)
+hours_spent_dict = {0.0:0}
+screen_name_courses_started = {}
+screen_name_courses_finished = {}
+screen_name_hours_spent = {}
 with open ('../../data/EarthSciences_ResGeo202_Spring2015_survey_responses.csv', 'r') as csvfile :
 	lines = csv.reader(csvfile, delimiter = ',', quotechar = '"')
 	for line in lines :
@@ -43,7 +46,27 @@ with open ('../../data/EarthSciences_ResGeo202_Spring2015_survey_responses.csv',
 				if num_course in courses_finished_dict :
 					courses_finished_dict[num_course] += 1
 				else :
-					courses_finished_dict[num_course] = 1			
+					courses_finished_dict[num_course] = 1	
+
+		# How many hours are you planning to spend on this course each week?
+		if line[0] == "SV_dhGmpvbbuyNZEA5" and line[2] == "Q1.3" :
+			if line[1] == "R_e3UzozQ3Q9TV7FL" :
+				print line
+			if line[4] == "" :
+				survey_id_hours_spent[line[1]] = 0.0
+				hours_spent_dict[0.0] += 1
+			# elif "." in line[4] or "," in line[4]:
+			# 	print line
+			else :
+				if "," in line[4] :
+					line[4] = line[4].replace(",", ".")
+				hours = float(line[4])
+				survey_id_hours_spent[line[1]] = hours
+				if hours in hours_spent_dict :
+					hours_spent_dict[hours] += 1
+				else :
+					hours_spent_dict[hours] = 1	
+					
 # print courses_started_dict
 # print len(survey_id_courses_started)
 # print courses_finished_dict	
@@ -56,7 +79,8 @@ with open('../../data/EarthSciences_ResGeo202_Spring2015_survey_response_metadat
 			screen_name_courses_started[line[2]] = survey_id_courses_started[line[1]]
 		if line[0] == "SV_dhGmpvbbuyNZEA5" and line[1] in survey_id_courses_finished :
 			screen_name_courses_finished[line[2]] = survey_id_courses_finished[line[1]]
-
+		if line[0] == "SV_dhGmpvbbuyNZEA5" and line[1] in survey_id_hours_spent :
+			screen_name_hours_spent[line[2]] = survey_id_hours_spent[line[1]]
 # print len(screen_name_courses_started)
 # print len(screen_name_courses_finished)
 count = 0
